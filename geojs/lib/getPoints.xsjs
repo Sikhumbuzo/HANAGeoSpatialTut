f31605d75e35ae05d826dc7224d35190aa3bc0ba
XSA_SKHU <sikhumbuzo.dlamini@sap.com>
2017-02-22 14:07:25 +0000

    FINALLY!!!!

--- geojs/lib/getPoints.xsjs
+++ geojs/lib/getPoints.xsjs
@@ -1,26 +1,32 @@
 try{
 	var connection = $.db.getConnection();
-	var preStatement = connection.prepareStatement('SELECT "CITY_COORDINATES".ST_AsGeoJson() as "LOCATION" FROM "0CT51OO89LZIPY9I_HANAGEOSPATIALTUT_HDI_CONTAINER"."HANAGeoSpatialTut.geodb::geocds.geodata"');
+	//var connection = $.hdb.getConnection();
+	var preStatement = connection.prepareStatement('SELECT "CITY_COORDINATES".ST_AsGeoJSON() as "LOCATION" FROM "0CT51OO89LZIPY9I_HANAGEOSPATIALTUT_HDI_CONTAINER"."HANAGeoSpatialTut.geodb::geocds.geodata"');
 	//var query = 'SELECT "CITY_COORDINATES".ST_AsGeoJson() AS "LOCATION" ' +
 	//'FROM "0CT51OO89LZIPY9I_HANAGEOSPATIALTUT_HDI_CONTAINER"."HANAGeoSpatialTut.geodb::geocds.geodata"';
 	var results = preStatement.executeQuery();
+	//var results = connection.executeQuery(query);
+	var response = {type: "FeatureCollection"};
 	
-	var response = {type: "GeoJsonLayer"};
 	response.features = [];
-	
-	while(results.next()){
-		
+	var body = {};
+	//var row = results[0];                 
+	   
+	//var items;
+	while(results.next()) {            
+		//items += results[row].LOCATION; 
+		//body = {Spots : results[i].toString()};Array([val])
 		response.features.push({type: "Feature", geometry: JSON.parse(results.getString(1))});
-		
+		//body = {geometry: results.getString(1)};
 	}
 	
+	//var obj = JSON.parse(items);  
 //	response.properties = {};
 	connection.close();
-	preStatement.close();
-	results.close();
+	//preStatement.close();
+	//results.close();
 	
 	$.response.contentType = "application/json";
-	//$.response.headers.set("Access-Control-Allow-Origin","*");
 	$.response.setBody(JSON.stringify(response));	
 	$.response.setStatus = $.net.http.OK;
 	
--- geoui/resources/webapp/controller/Main.controller.js
+++ geoui/resources/webapp/controller/Main.controller.js
@@ -7,14 +7,17 @@ sap.ui.define([
 	return Controller.extend("com.sap.geo.ui.controller.Main", {
 			onInit: function(){
 			
-			var oModel = new sap.ui.model.json.JSONModel("https://vhhan200.pro.coil:51041/getPoints.xsjs");
-			this.getView().setModel(oModel, "locations");
+			/*var oModel = new sap.ui.model.json.JSONModel();
+			oModel.setData(this.oData);
+			this.getView().setModel(oModel);
 			
-			var oDeviceModel = new sap.ui.model.json.JSONModel(Device);
+			/*var oDeviceModel = new sap.ui.model.json.JSONModel(Device);
 			oDeviceModel.setDefaultBindingMode("OneWay");
 			this.getView().setModel(oDeviceModel, "device");
+			*/
 			
 			var oGeoMap = this.getView().byId("vbi");
+			oGeoMap.addGeoJsonLayer(this.addFeatureCollection());
 			var oMapConfig = {
 				"MapProvider": [{
 					"name": "HEREMAPS",
@@ -26,10 +29,10 @@ sap.ui.define([
 					"copyright": "HERE Maps",
 					"Source": [{
 						"id": "s1",
-						"url": "https://1.base.maps.cit.api.here.com/maptile/2.1/maptile/newest/normal.day/{LOD}/{X}/{Y}/256/png8?app_id=Mv1sZKBYuOL0uLAPcrRy&app_code=lglFX4tb_IwBnHuvVsNT6w"
+						"url": "https://1.base.maps.cit.api.here.com/maptile/2.1/maptile/newest/normal.day.grey/{LOD}/{X}/{Y}/256/png8?app_id=Mv1sZKBYuOL0uLAPcrRy&app_code=lglFX4tb_IwBnHuvVsNT6w"
 					}, {
 						"id": "s2",
-						"url": "https://2.base.maps.cit.api.here.com/maptile/2.1/maptile/newest/normal.day/{LOD}/{X}/{Y}/256/png8?app_id=Mv1sZKBYuOL0uLAPcrRy&app_code=lglFX4tb_IwBnHuvVsNT6w"
+						"url": "https://2.base.maps.cit.api.here.com/maptile/2.1/maptile/newest/normal.day.grey/{LOD}/{X}/{Y}/256/png8?app_id=Mv1sZKBYuOL0uLAPcrRy&app_code=lglFX4tb_IwBnHuvVsNT6w"
 					}]
 				}],
 				"MapLayerStacks": [{
@@ -44,6 +47,22 @@ sap.ui.define([
 			};
 			oGeoMap.setMapConfiguration(oMapConfig);
 			oGeoMap.setRefMapLayerStack("DEFAULT");
+		},
+		addFeatureCollection : function (){
+			var gJson = new sap.ui.vbm.GeoJsonLayer({
+				srcURL: "https://vhhan200.pro.coil:51041/getPoints.xsjs",
+				items: {
+					path: "/Features",
+					template: new sap.ui.vbm.Feature(
+													{
+														color: "rgba(182,217,87,0.6)",
+														tooltip: "Testing"
+													}
+						)
+				}
+			});
+			return gJson;
 		}
+		
 	});
 });
